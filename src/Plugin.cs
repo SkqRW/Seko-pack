af87d35df87e287fee43d99b025c750fd3386e74
Skq <skq.rain@gmail.com>
2026-01-14 08:13:45 -0500

    add fisobs depedency

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using System;
 using System.Security.Permissions;
 
 // Allows access to private members
@@ -9,7 +10,7 @@ using System.Security.Permissions;
 
 namespace SekoPack;
 
-[BepInPlugin(MODID, MODNAME, MODVERSION)]
+[BepInPlugin(MODID, MODNAME, MODVERSION), BepInDependency("io.github.dual.fisobs")]
 public class ModPlugin : BaseUnityPlugin
 {
     public static new ManualLogSource Logger;
@@ -17,22 +18,39 @@ public class ModPlugin : BaseUnityPlugin
     public const string MODID = "seko.pack";
     public const string MODNAME = "Seko Pack";
     public const string MODVERSION = "000";
-    bool IsInit;
+    static bool IsInit;
 
     public void OnEnable()
     {
         Logger = base.Logger;
         On.RainWorld.OnModsInit += OnModsInit;
+        RegisterFisobs(); 
     }
 
-    private void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    private static void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
     {
         orig(self);
 
         if (IsInit) return;
         IsInit = true;
 
-        // Initialize assets, your mod config, and anything that uses RainWorld here
         Logger.LogDebug($"[{MODID}] {MODNAME} Hello world! {MODVERSION}");
+
+        LoadResources();
+    }
+
+    private static void RegisterFisobs()
+    {
+        
+        Logger.LogDebug($"[{MODID}] {MODNAME} Registering Fisobs...");
+
+
+
+        Logger.LogDebug($"[{MODID}] {MODNAME} Fisobs registered.");
+    }
+
+    private static void LoadResources()
+    {
+        
     }
 }
